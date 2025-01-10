61e3c72373412a02bcfa3995101706ba96e1d5e0
HagerMagdy2 <hager9671@gmail.com>
2025-01-10 18:26:10 +0200

    cart ui

--- src/app/carts/components/cart/cart.component.ts
+++ src/app/carts/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
-
+  cart:any[]=[]
+  constructor(private router:Router){}
+  onCheckoutClick() {
+    alert('Checkout successful! Redirecting to home page...');
+    
+  this.router.navigate(['/']);
+  }
+   getTotalPrice(card: any): number {
+  //  const total = card.reduce((total, item) => total + item.price, 0);
+  return Math.round(1 * 100) / 100;
+  }
+  RemoveFromCard(id:number){
+    // console.log(this.card[id])
+    // this.card.splice(id-1, 1);
+  }
 }
